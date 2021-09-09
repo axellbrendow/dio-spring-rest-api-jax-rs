7b07bc6cccb4a5f989f1b108100a377faeeae162
axell-brendow <breno.axel@gmail.com>
2021-09-09 10:35:36 -0300

    Update SoldierController using dtos and mappers

--- src/main/java/br/com/axellbrendow/diospringrestapijaxrs/controller/SoldierController.java
+++ src/main/java/br/com/axellbrendow/diospringrestapijaxrs/controller/SoldierController.java
@@ -1,6 +1,9 @@
 package br.com.axellbrendow.diospringrestapijaxrs.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.axellbrendow.diospringrestapijaxrs.controller.request.SoldierEditRequest;
+import br.com.axellbrendow.diospringrestapijaxrs.controller.response.SoldierResponse;
 import br.com.axellbrendow.diospringrestapijaxrs.dto.Soldier;
 import br.com.axellbrendow.diospringrestapijaxrs.service.SoldierService;
 
@@ -22,30 +26,35 @@ import br.com.axellbrendow.diospringrestapijaxrs.service.SoldierService;
 @RequestMapping("/v1/soldier")
 public class SoldierController {
     private SoldierService service;
+    private ObjectMapper mapper;
 
-    public SoldierController(SoldierService service) {
+    public SoldierController(SoldierService service, ObjectMapper mapper) {
         this.service = service;
+        this.mapper = mapper;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Soldier> get() {
-        return ResponseEntity.ok(service.get());
+    public ResponseEntity<SoldierResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
     }
 
     @GetMapping
-    public ResponseEntity<List<Soldier>> getAll() {
-        return ResponseEntity.ok(service.getAll());
+    public ResponseEntity<List<SoldierResponse>> getAll() {
+        var soldiers = service.findAll().stream()
+            .map(it -> mapper.convertValue(it, SoldierResponse.class))
+            .collect(Collectors.toList());
+        return ResponseEntity.ok(soldiers);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public ResponseEntity<Soldier> create(@RequestBody Soldier soldier) {
-        return ResponseEntity.ok(service.create(soldier));
+    public void create(@RequestBody Soldier soldier) {
+        service.save(soldier);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Soldier> update(
-        @PathVariable String id,
+        @PathVariable Long id,
         @RequestBody SoldierEditRequest editRequest
     ) {
         service.update(id, editRequest);
@@ -54,7 +63,7 @@ public class SoldierController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable String id) {
+    public void delete(@PathVariable Long id) {
         service.delete(id);
     }
 }
