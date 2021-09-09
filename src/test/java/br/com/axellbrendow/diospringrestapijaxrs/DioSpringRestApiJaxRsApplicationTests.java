544ddc12e8227dff2070d3f138b838683c070098
axell-brendow <breno.axel@gmail.com>
2021-09-09 08:24:44 -0300

    Upload default test

---
+++ src/test/java/br/com/axellbrendow/diospringrestapijaxrs/DioSpringRestApiJaxRsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diospringrestapijaxrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringRestApiJaxRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
