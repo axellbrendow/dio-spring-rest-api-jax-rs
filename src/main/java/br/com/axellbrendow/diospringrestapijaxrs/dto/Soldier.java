1f19298c8812156da410dedaf50d64d5aaf9cdb1
axell-brendow <breno.axel@gmail.com>
2021-09-09 08:42:48 -0300

    Add id and status to dto

--- src/main/java/br/com/axellbrendow/diospringrestapijaxrs/dto/Soldier.java
+++ src/main/java/br/com/axellbrendow/diospringrestapijaxrs/dto/Soldier.java
@@ -1,25 +1,49 @@
 package br.com.axellbrendow.diospringrestapijaxrs.dto;
 
 public class Soldier {
+    private Long id;
     private String name;
     private String race;
     private String weapon;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getRace() {
         return race;
     }
+
     public void setRace(String race) {
         this.race = race;
     }
+
     public String getWeapon() {
         return weapon;
     }
+
     public void setWeapon(String weapon) {
         this.weapon = weapon;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
