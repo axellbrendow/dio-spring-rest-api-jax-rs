d04cd273eb36c62e97f3d896a118e37ca075aff9
axell-brendow <breno.axel@gmail.com>
2021-09-09 08:25:16 -0300

    Add main method

---
+++ src/main/java/br/com/axellbrendow/diospringrestapijaxrs/DioSpringRestApiJaxRsApplication.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diospringrestapijaxrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringRestApiJaxRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringRestApiJaxRsApplication.class, args);
+	}
+
+}
